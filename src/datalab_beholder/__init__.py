f9af95914160e43d892fa8e68d95aecdd49dd34d
Matthew Evans <git@ml-evs.science>
2026-09-03 22:26:24 +0100

    Use importlib to set dynamic version

--- src/datalab_beholder/__init__.py
+++ src/datalab_beholder/__init__.py
@@ -1,3 +1,5 @@
 """datalab-beholder: Filesystem watcher daemon for datalab instances."""
 
-__version__ = "0.1.0"
+from importlib.metadata import version
+
+__version__ = version("datalab-beholder")
